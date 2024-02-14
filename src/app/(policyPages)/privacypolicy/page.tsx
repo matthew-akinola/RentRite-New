86abbecae239e65e5379cdda6489fc62ec35b064
jackfros-glitch <hirekaanm3@gmail.com>
2024-02-14 05:03:01 +0100

    feat:PrivacyPolicy Page.

---
+++ src/app/(policyPages)/privacypolicy/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-white text-[#161518]'>
+     <h1 className=' font-medium text-[#161518] text-[40px] text-center mt-5' >Rentrite Privacy Policy</h1>
+
+     <div className='text-[#161518] px-12 my-8 max-w-[1032px] mx-auto'>
+        <h2 className='mb-5 font-semibold text-xl'>Rentrite Privacy Policy</h2>
+        <p className=' font-normal text-base'>
+        Nigeria Property Centre (‘NPC’) respects your privacy and is committed to protecting personal information collected from you through our website. This privacy policy governs thLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum eu tellus ut efficitur. Etiam et justo consequat, commodo ligula quis, accumsan nibh. Sed purus nisl, viverra nec elit vel, sagittis ornare metus. Etiam fermentum.
+        </p>
+     </div>
+     <div className='text-[#161518] px-12 my-8 max-w-[1032px] mx-auto' >
+        <h2 className='mb-5 font-semibold text-xl' >Rentrite Privacy Policy</h2>
+        <p className=' font-normal text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum eu tellus ut efficitur. Etiam et justo consequat, commodo ligula quis, accumsan nibh. Sed purus nisl, viverra nec elit vel, sagittis ornare metus. Etiam fermentum velit eget neque eleifend ultrices. Nulla cursus nulla sed ligula consectetur, at faucibus enim consectetur. Sed fermentum libero sed ipsum congue, et pharetra lectus aliquet. Sed a cursus arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum non turpis vehicula, eleifend diam ac, bibendum odio. Nullam in nisi ante. Suspendisse ullamcorper dictum felis at laoreet. Sed porttitor tellus in justo maximus tincidunt. Pellentesque lobortis lacus eget ante euismod, eget imperdiet risus elementum. Ut commodo arcu sed placerat eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+     </div>
+     <div className='text-[#161518] px-12 my-8 max-w-[1032px] mx-auto'>
+        <h2 className='mb-5 font-semibold text-xl' >Rentrite Privacy Policy</h2>
+        <p className=' font-normal text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum eu tellus ut efficitur. Etiam et justo consequat, commodo ligula quis, accumsan nibh. Sed purus nisl, viverra nec elit vel, sagittis ornare metus. Etiam fermentum velit eget neque eleifend ultrices. Nulla cursus nulla sed ligula consectetur, at faucibus enim consectetur. Sed fermentum libero sed ipsum congue, et pharetra lectus aliquet. Sed a cursus arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum non turpis vehicula.</p>
+     </div>
+     <div className='text-[#161518] px-12 my-8 max-w-[1032px] mx-auto'>
+        <h2 className='mb-5 font-semibold text-xl' >Rentrite Privacy Policy</h2>
+        <p className=' font-normal text-base'>Nigeria Property Centre (‘NPC’) respects your privacy and is committed to protecting personal information collected from you through our website. This privacy policy governs thLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum eu tellus ut efficitur. Etiam et justo consequat, commodo ligula quis, accumsan nibh. Sed purus nisl, viverra nec elit vel, sagittis ornare metus. Etiam fermentum.</p>
+     </div>
+     <div className='text-[#161518] px-12 my-8 max-w-[1032px] mx-auto'>
+        <h2 className='mb-5 font-semibold text-xl'>Rentrite Privacy Policy</h2>
+        <p className=' font-normal text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum eu tellus ut efficitur. Etiam et justo consequat, commodo ligula quis, accumsan nibh. Sed purus nisl, viverra nec elit vel, sagittis ornare metus. Etiam fermentum velit eget neque eleifend ultrices. Nulla cursus nulla sed ligula consectetur, at faucibus enim consectetur. Sed fermentum libero sed ipsum congue, et pharetra lectus aliquet. Sed a cursus arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum non turpis vehicula, eleifend diam ac, bibendum odio. Nullam in nisi ante. Suspendisse ullamcorper dictum felis at laoreet. Sed porttitor tellus in justo maximus tincidunt. Pellentesque lobortis lacus eget ante euismod, eget imperdiet risus elementum. Ut commodo arcu sed placerat eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+     </div>
+     <div className='text-[#161518] px-12 my-8 max-w-[1032px] mx-auto'>
+        <h2 className='mb-5 font-semibold text-xl'>Rentrite Privacy Policy</h2>
+        <p className=' font-normal text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum eu tellus ut efficitur. Etiam et justo consequat, commodo ligula quis, accumsan nibh. Sed purus nisl, viverra nec elit vel, sagittis ornare metus. Etiam fermentum velit eget neque eleifend ultrices. Nulla cursus nulla sed ligula consectetur, at faucibus enim consectetur. Sed fermentum libero sed ipsum congue, et pharetra lectus aliquet. Sed a cursus arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum non turpis vehicula.</p>
+     </div>
+    </div>
+  )
+}
+
+export default page
